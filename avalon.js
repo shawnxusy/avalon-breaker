92bc8ce92d0113de149599ff4f2f348e55bef4dc
Shawn Xu <shawn.xusy@gmail.com>
2015-12-15 13:45:58 -0800

    set up test case and wrote rule 1. Test case works fine.

--- avalon.js
+++ avalon.js
@@ -1,5 +1,4 @@
-(function() {
-'use strict';
+
 
 /*
     Game resolver for Avalon, a card game similar to resistance, mafia, werewolve.
@@ -128,7 +127,7 @@
         function Mission(missionNumber, team, result) {
             this.missionNumber = missionNumber;
             this.team = team;
-            this.result = result;
+            this.result = result; //FAILED || SUCCEEDED
         }
 
         function Testify(missionNumber, direction, accuser, accusee) {
@@ -160,7 +159,7 @@
 
                             if (villainCount > 0) {
                                 console.log("Merlin approved " + villainCount + " villains in vote number " +
-                                            vote.missionNumber + ". Likelihood *= " + likelihood);
+                                            vote.missionNumber + ". Likelihood drops to " + likelihood);
                             }
                         }
                 }
@@ -185,7 +184,7 @@
 
                     if (villainCount > 0) {
                         console.log("Merlin proposed " + villainCount + " villains in propose number " +
-                                propose.missionNumber + ". Likelihood *= " + likelihood);
+                                propose.missionNumber + ". Likelihood drops to " + likelihood);
                     }
                 }
             });
@@ -201,16 +200,16 @@
         function villainProposeAnotherInRoundFour(proposes, assumption) {
             var likelihood = 1;
 
-            if (proposes.length >= 3) {
+            if (proposes.length >= 4) {
                 var propose = proposes[3];
 
                 if (_.indexOf(assumption.villains, propose.proposer) !== -1) {
                     var otherVillainCount = _.intersection(propose.proposee, assumption.villains).length - 1;
-                    likelihood = otherVillainCount > 0 ? 1.2 : 1;
+                    likelihood *= (otherVillainCount > 0) ? 1.2 : 1;
 
                     if (otherVillainCount > 0) {
                         console.log("In proposal 4, a villain proposed " + otherVillainCount + " villain(s). " +
-                                    "Likelihood *= " + likelihood);
+                                    "Likelihood increased to " + likelihood);
                     }
                 }
             }
@@ -221,11 +220,33 @@
         /*
             Rule 4: In a mission that fails, there can't be no villains
             - Likelihood: * 0
-            - Edge case: null
+            - Edge case: if mission 4 fails, there must be 2 villains
          */
         function villainsInFailedMissions(missions, assumption) {
+            var likelihood = 1;
 
+            _.each(missions, function(mission) {
+                if ((mission.missionNumber !== 3) && (mission.result === "FAILED")) {
+                    if (_.intersection(mission.team, assumption.villains).length < 1) {
+                        likelihood *= 0;
+                        console.log("In a mission, a villain is mixed in, likelihood dropped to 0.");
+                    }
+                } else if ((mission.missionNumber === 3) && (mission.result === "FAILED")) {
+                    if (_.intersection(mission.team, assumption.villains).length < 2) {
+                        likelihood *= 0;
+                        console.log("In mission #4, two villains are mixed in, likelihood dropped to 0.");
+                    }
+                }
+            });
+
+            return likelihood;
         }
+
+        /*
+            Rule 5: Villains tend to reject proposes that does not contain villains
+            - Likelihood: Increasing by each round (base: 150%)
+            - Edge case: null
+         */
 
     /*
         ---------------------------
@@ -285,7 +306,7 @@
         var votes = [];
         var missions = [];
 
-        var possibleCombo = [];
+        var resultHash = {}; // {315268407: 0.8, 586472130: 0.4}
 
         /*
             Generate possible assumptions and calculate likelihood for each.
@@ -293,7 +314,6 @@
             only generate those with a higher possibility.
          */
         function runDeduction() {
-            var resultHash = {}; // {315268407: 0.8, 586472130: 0.4}
 
             var initialArray = []; // [0,1,2,3,4,...]
             for (var i = 0; i < noOfPlayers; i++) {
@@ -307,7 +327,7 @@
 
             _.each(permutations, function(permutation) {
                 // function Assumption(merlin, percival, morgana, assasin, mordred, dummy)
-                // omg this is ugly
+                // OMG this is ugly
                 var merlin = permutation[0];
                 var percival = permutation[1];
                 var morgana = permutation[2];
@@ -326,6 +346,74 @@
                     resultHash[permutation.join("")] = likelihood;
                 }
             });
+
+
+
+        }
+
+    /*
+        ---------------------------
+        Test case
+        ---------------------------
+     */
+        // Simple case: bad guy do bad things
+        function testCase1() {
+            noOfPlayers = 7;
+            noOfVillains = 3;
+            noOfHeroes = 2;
+            noOfInnos = 2;
+            // 2: Merlin, 3: Assasin; 4: Percival; 5: Mordred; 6: Morgana;
+
+            // Round 1
+            var propose1 = new Propose(0, 2, [4,5,7]);
+            var vote1 = new Vote(0, 2, [2,7], [1,3,4,5,6]);
+            var propose2 = new Propose(0, 3, [3,5,1]);
+            var vote2 = new Vote(0, 3, [3,5,6], [2,4,7,1]);
+            var propose3 = new Propose(0, 4, [2,4,5]);
+            var vote3 = new Vote(0, 4, [2,3,4,5], [1,6,7]);
+            var mission1 = new Mission(0, [2,4,5], 'FAILED');
+
+            // Round 2
+            var propose4 = new Propose(1, 5, [5,6,7]);
+            var vote4 = new Vote(1, 5, [5,6], [1,2,3,4,7]);
+            var propose5 = new Propose(1, 6, [4,6,7]);
+            var vote5 = new Vote(1, 6, [3,6], [1,2,4,5,7]);
+            var propose6 = new Propose(1, 7, [1,2,7]);
+            var vote6 = new Vote(1, 7, [1,2,4,7], [3,5,6]);
+            var mission2 = new Mission(1, [1,2,4,7], 'SUCCEEDED');
+
+            // Round 3
+            var propose7 = new Propose(2, 1, [1,3,4,7]);
+            var vote7 = new Vote(2, 1, [3,1,6], [2,4,5,7]);
+            var propose8 = new Propose(2, 2, [1,2,7,4]);
+            var vote8 = new Vote(2, 2, [1,2,4,7], [3,5,6]);
+            var mission3 = new Mission(2, [1,2,4,7], 'SUCCEEDED');
+
+            missions.push(mission1);
+            missions.push(mission2);
+            missions.push(mission3);
+
+            proposes.push(propose1);
+            proposes.push(propose2);
+            proposes.push(propose3);
+            proposes.push(propose4);
+            proposes.push(propose5);
+            proposes.push(propose6);
+            proposes.push(propose7);
+            proposes.push(propose8);
+
+            votes.push(vote1);
+            votes.push(vote2);
+            votes.push(vote3);
+            votes.push(vote4);
+            votes.push(vote5);
+            votes.push(vote6);
+            votes.push(vote7);
+            votes.push(vote8);
+
+            runDeduction();
+
+            console.log(resultHash);
 
         }
 
@@ -372,5 +460,3 @@
 
           return deducted;
       }
-
-})();

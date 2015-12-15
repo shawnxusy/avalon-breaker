ca817fa43ce4841b801b3a9a7f28a18cf0e00869
Shawn Xu <shawn.xusy@gmail.com>
2015-12-14 17:31:27 -0800

    Added deduction and more rules

--- avalon.js
+++ avalon.js
@@ -22,12 +22,15 @@
         high: 1.1
     };
 
+    var possibleThreshold = 0.5;
+
     /*
         ---------------------------
         General class (role) settings
         ---------------------------
      */
         function Player() {
+            this.id = null;
             this.isHero = 0; // Possibility of being morgana, same below
             this.isVillain = 0;
             this.isInnocent = 1;
@@ -109,23 +112,30 @@
         !important: missionNumber starts at 0 (round 1 -> missionNumber : 0)
         ---------------------------
      */
-        function Propose() {
-            this.missionNumber = 0;
-            this.proposer = null;
-            this.proposee = [];
+        function Propose(missionNumber, proposer, proposee) {
+            this.missionNumber = missionNumber;
+            this.proposer = proposer;
+            this.proposee = proposee; // []
         }
 
-        function Vote() {
-            this.missionNumber = 0;
-            this.proposee = [];
-            this.agreed = [];
-            this.disagreed = [];
+        function Vote(missionNumber, proposee, agreed, disagreed) {
+            this.missionNumber = missionNumber;
+            this.proposee = proposee; // []
+            this.agreed = agreed; // []
+            this.disagreed = disagreed; // []
         }
 
-        function Mission() {
-            this.missionNumber = 0;
-            this.team = [];
-            this.result = null;
+        function Mission(missionNumber, team, result) {
+            this.missionNumber = missionNumber;
+            this.team = team;
+            this.result = result;
+        }
+
+        function Testify(missionNumber, direction, accuser, accusee) {
+            this.missionNumber = missionNumber;
+            this.direction = direction; // "positive" / "negative"
+            this.accuser = accuser;
+            this.accusee = accusee; // []
         }
     /*
         ---------------------------
@@ -133,22 +143,25 @@
         ---------------------------
      */
 
-    /*
-        Rule 1: Merlin should not approve any villain except Mordred in a vote
-        - Likelihood: Increased by the number of villains in votes (base 30%)
-        - Edge case: Game 4, where villain tolerance is increased by 1
-        - Edge case: Mordred's existence.
-     */
+        /*
+            Rule 1: Merlin should not approve any villain except Mordred in a vote
+            - Likelihood: Decreasing by the number of villains in votes (base *30%)
+            - Edge case: Game 4, where villain tolerance is increased by 1
+            - Edge case: Mordred's existence.
+         */
         function merlinApproveVillainInVote(votes, assumption) {
             var likelihood = 1;
 
             _.each(votes, function(vote) {
-                if (vote.missionNumber != 4) {
+                if (vote.missionNumber != 3) {
                     if (_.indexOf(vote.agreed, assumption.merlin) !== -1) {
                             var villainCount = _.intersection(vote.proposee, assumption.visibleVillains).length;
                             likelihood *= calcDeduction(0.3, "non-linear", villainCount, null, "high");
-                            console.log("Merlin approved " + villainCount + " villains in vote number " +
-                                        vote.missionNumber + ". Likelihood *= " + likelihood);
+
+                            if (villainCount > 0) {
+                                console.log("Merlin approved " + villainCount + " villains in vote number " +
+                                            vote.missionNumber + ". Likelihood *= " + likelihood);
+                            }
                         }
                 }
             });
@@ -156,12 +169,12 @@
             return likelihood;
         }
 
-    /*
-        Rule 2: As the game advances, it is less likely that Merlin proposes villains
-        - Likelihood: Increasing by each round (base 80%), and each number of villain
-        - Edge case: Game 4, where there is a slightly higher chance
-        - Edge case: Mordred's existence
-     */
+        /*
+            Rule 2: As the game advances, it is less likely that Merlin proposes villains
+            - Likelihood: Decreasing by each round (base *80%), and each number of villain
+            - Edge case: Game 4, where there is a slightly higher chance
+            - Edge case: Mordred's existence
+         */
         function merlinProposeVillains(proposes, assumption) {
             var likelihood = 1;
 
@@ -169,44 +182,138 @@
                 if (propose.proposer === assumption.merlin) {
                     var villainCount = _.intersection(propose.proposee, assumption.visibleVillains).length;
                     likelihood *= calcDeduction(0.8, "non-linear", villainCount, propose.missionNumber, "high");
-                    console.log("Merlin proposed " + villainCount + " villains in propose number " +
+
+                    if (villainCount > 0) {
+                        console.log("Merlin proposed " + villainCount + " villains in propose number " +
                                 propose.missionNumber + ". Likelihood *= " + likelihood);
+                    }
                 }
             });
 
             return likelihood;
         }
 
-    /*
-        Rule 3: 
-        - Likelihood:
-        - Edge case:
-     */
+        /*
+            Rule 3: In proposal 4, a villain is tend to propose another villain(s)
+            - Likelihood: * 120%
+            - Edge case: null
+         */
+        function villainProposeAnotherInRoundFour(proposes, assumption) {
+            var likelihood = 1;
+
+            if (proposes.length >= 3) {
+                var propose = proposes[3];
+
+                if (_.indexOf(assumption.villains, propose.proposer) !== -1) {
+                    var otherVillainCount = _.intersection(propose.proposee, assumption.villains).length - 1;
+                    likelihood = otherVillainCount > 0 ? 1.2 : 1;
+
+                    if (otherVillainCount > 0) {
+                        console.log("In proposal 4, a villain proposed " + otherVillainCount + " villain(s). " +
+                                    "Likelihood *= " + likelihood);
+                    }
+                }
+            }
+
+            return likelihood;
+        }
+
+        /*
+            Rule 4: In a mission that fails, there can't be no villains
+            - Likelihood: * 0
+            - Edge case: null
+         */
+        function villainsInFailedMissions(missions, assumption) {
+
+        }
 
     /*
         ---------------------------
-        Assumption + deduction
+        Assumption
         ---------------------------
      */
-     // assumption {merlin: 1; mordred: 3; ...}
+        /*
+            Assumption class to store assumptions for running deduction
+            @example var assumption = new Assumption(1,3,6,2,4,7);
+            @param playerIds
+         */
+        function Assumption(merlin, percival, morgana, assasin, mordred, dummy) {
+            this.merlin = merlin;
+            this.percival = percival;
+            this.morgana = morgana;
+            this.assasin = assasin;
+            this.mordred = mordred;
+            this.dummy = dummy;
+            this.villains = [morgana, assasin, mordred, dummy];
+            this.visibleVillains = [morgana, assasin, dummy];
+            this.heroes = [merlin, percival];
+        }
 
-     /*
+    /*
         ---------------------------
-        Recording actual game progress
+        UI connectors
         ---------------------------
-      */
-      function Progress() {
-          this.proposes = [];
-          this.votes = [];
-          this.missions = [];
-      }
+     */
+        function onPropose(proposer, proposee) {
+            var propose = new Propose(missionNumber, proposer, proposee);
+            proposes.push(propose);
+            runDeduction();
+        }
 
-      /*
-          ---------------------------
-          Helper methods
-          ---------------------------
-       */
-      /*
+        function onVote() {
+            var vote = new Vote(missionNumber, proposee, agreed, disagreed);
+            votes.push(vote);
+            runDeduction();
+        }
+
+        function onMission() {
+            var mission = new Mission(missionNumber, team, result);
+            missions.push(mission);
+            runDeduction();
+
+            missionNumber++;
+        }
+
+
+    /*
+        ---------------------------
+        Game client (main)
+        ---------------------------
+     */
+        var missionNumber = 0;
+        var proposes = [];
+        var votes = [];
+        var missions = [];
+
+        var possibleCombo = [];
+
+        /*
+            Generate possible assumptions and calculate likelihood for each.
+            It's possible to do optimization here: instead of generating all patterns,
+            only generate those with a higher possibility.
+         */
+        function runDeduction() {
+            var initialArray = []; // [0,1,2,3,4,...]
+            for (var i = 0; i < noOfPlayers; i++) {
+                initialArray.push(i);
+            }
+
+            // Permutation for [0,1,2,3] -> [ [0,1,2,3], [1,0,2,3], [0,1,3,2],... ]
+            var permutation = initialArray.reduce(function permute(res, item, key, arr) {
+                return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1)).reduce(permute, []).map(function(perm) {
+                    return [item].concat(perm); }) || item);
+                }, []);
+
+            var assumption = new Assumption();  // function Assumption(merlin, percival, morgana, assasin, mordred, dummy)
+        }
+
+    /*
+        ---------------------------
+        Helper methods
+        ---------------------------
+    */
+
+        /*
             Helper to calculate deduction of possibilities
             @param base: the base percentage given all the rest default
             @method: specify "linear" or "non-linear"
